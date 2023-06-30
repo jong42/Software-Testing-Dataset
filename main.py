8a4dc0bec7e4294cc3b764a1331fbd0cab642cb4
Jonas <jonasguetter@gmx.de>
2023-06-30 18:33:44 +0200

    Add main.py with testcase selection pipeline

---
+++ main.py
@@ -0,0 +1,30 @@
+import re
+import json
+from select import create_dependency_graph, get_affected_files
+
+codebase_path = "/home/jonas/Desktop/pydeps_local"
+commit_path = "commit.txt"
+graph_path = "dep_graph.json"
+
+create_dependency_graph(codebase_path, graph_path)
+
+# Extract filenames from commit
+filenames = []
+with open(commit_path, "r") as f:
+    lines = f.readlines()
+    lines = lines[6:-1]
+    for line in lines:
+        filename = re.findall('/.+\.py',line)[0][1:]
+        filenames.append(filename)
+
+
+# Load graph into Dict
+with open(graph_path, "r") as f:
+    dependency_graph = json.load(f)
+
+# query graph for dependent files.
+selected_tests = []
+for filename in filenames:
+    affected_files = get_affected_files(filename, dependency_graph)
+    selected_tests.append(affected_files)  # Assumption: Test files have same names as the codebase files
+    selected_tests = list(set(selected_tests))  # to remove duplicate entries
