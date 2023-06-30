786ff99431a8624b7e1361e8e0e91a5443367cd5
Jonas <jonasguetter@gmx.de>
2023-06-30 10:42:23 +0200

    Write function that creates a dependency graph of a codebase

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from pydeps.pydeps import call_pydeps
 
 
-def create_dependency_graph(path:str):
+def create_dependency_graph(inpath:str, outpath:str):
     """
     Creates a dependency graph of a codebase at a given location
-    :param path: string. The location of the codebase
+    :param inpath: string. The location of the codebase
+    :param outpath: string. The location where the output file should be written to
     :return:
     """
-    call_pydeps(path)
+    call_pydeps(inpath, show_deps=True, no_output=True, deps_out=outpath, )
 
 if __name__ == '__main__':
     path = "/home/jonas/Desktop/pydeps_local/pydeps_local"
-    create_dependency_graph(path)
+    outpath = "output.json"
+    create_dependency_graph(path, outpath)
