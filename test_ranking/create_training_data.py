8d5f7dec7bf3009103c2675b2e3b043838a40b65
Jonas <jonasguetter@gmx.de>
2023-07-04 18:14:25 +0200

    Add function to perform tests

--- test_ranking/create_training_data.py
+++ test_ranking/create_training_data.py
@@ -14,13 +14,25 @@ def list_projects():
 def clone_project(url:str, outdir:str):
     subprocess.run(['git', 'clone', url, outdir])
 
+def remove_project(dir:str):
+    """
+    Remove a directory from the file system
+    :param dir:
+    :return:
+    """
+    pass
+
+#execute the tests
+def execute_tests(path:str):
+    subprocess.run(['pytest', path])
+
 if __name__ == '__main__':
     list_projects()
     url = 'https://gitlab.com/megabyte-labs/templates/python-cli'
     outdir = '/home/jonas/Desktop/temp_repo'
-    clone_project(url, outdir)
+    #clone_project(url, outdir)
 
-#execute the tests
+
 
 
 #Make a subclass of project that can clone, run tests and delete the local clone
