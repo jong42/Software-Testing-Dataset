1436987d35b44c2acec0bd6fc471a2f8a0da85b6
Jonas <jonasguetter@gmx.de>
2023-07-04 18:10:12 +0200

    Add function to clone git projects

--- test_ranking/create_training_data.py
+++ test_ranking/create_training_data.py
@@ -1,11 +1,26 @@
 import gitlab
+import subprocess
 
 
-gl = gitlab.Gitlab()
+def list_projects():
+    gl = gitlab.Gitlab()
+    # list all the projects
+    projects = gl.projects.list(topic='pytest')
+    for project in projects:
+        print(project)
 
-# list all the projects
-projects = gl.projects.list(iterator=True, topic='pytest')
-for project in projects:
-    print(project)
-    break
 
+#clone those projects
+def clone_project(url:str, outdir:str):
+    subprocess.run(['git', 'clone', url, outdir])
+
+if __name__ == '__main__':
+    list_projects()
+    url = 'https://gitlab.com/megabyte-labs/templates/python-cli'
+    outdir = '/home/jonas/Desktop/temp_repo'
+    clone_project(url, outdir)
+
+#execute the tests
+
+
+#Make a subclass of project that can clone, run tests and delete the local clone
