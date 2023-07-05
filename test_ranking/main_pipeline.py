836a9b2a810b309a2bb13f8231e48a4c12d081cd
Jonas <jonasguetter@gmx.de>
2023-07-05 17:38:59 +0200

    Complete metric names and debug

--- test_ranking/main_pipeline.py
+++ test_ranking/main_pipeline.py
@@ -14,14 +14,34 @@ for url in repo_urls:
     clone_project(url, temp_repo_path)
     codebase_path = os.path.join(temp_repo_path,repo_name) # TODO: get repo name
     files = os.listdir(codebase_path)
+    # Filter for python files
+    files = [file for file in files if '.py' in file]
     # Get code metrics from repo, write them in a  dataframe
     abs_filepaths = [os.path.join(temp_repo_path,file) for file in files]
     code_metrics = get_static_features(abs_filepaths)
-    for filename,values in code_metrics:
-        # df.append([url,filename,values[], values['avg_cc']]) #TODO: insert all the metrics names
+    for filename in code_metrics:
+        df.append([url,
+                   filename,
+                   code_metrics[filename]['loc'],
+                   code_metrics[filename]['lloc'],
+                   code_metrics[filename]['sloc'],
+                   code_metrics[filename]['comments'],
+                   code_metrics[filename]['multi'],
+                   code_metrics[filename]['blank'],
+                   code_metrics[filename]['single_comments'],
+                   code_metrics[filename]['avg_cc']])
 
 # Delete repo
 remove_project(temp_repo_path)
 # Save dataframe as csv
-df = pd.DataFrame(df,columns=['url','filename','avg_cc']) # TODO: insert all metrics names
+df = pd.DataFrame(df, columns=['url',
+                               'filename',
+                               'loc',
+                               'lloc',
+                               'sloc',
+                               'comments',
+                               'multi',
+                               'blank',
+                               'single_comments',
+                               'avg_cc'])
 df.to_csv(outpath)
