0faba7272f52032c9864ba9d828e1f710e8a5df3
Jonas <jonasguetter@gmx.de>
2023-07-06 11:25:19 +0200

    Change temp repo path and reorder deleting of temp repo

--- std/main.py
+++ std/main.py
@@ -3,15 +3,13 @@ import pandas as pd
 from git_utils import list_files, get_project_urls, clone_project, remove_project
 from code_analysis import get_static_features
 
-temp_repo_path = '/home/jonas/Desktop/temp_repo'
+temp_repo_path = '../temp_repo'
 outpath = '../code_metrics.csv'
 df = []
+
 # Get names and urls of possible repos
 repo_names, repo_urls = get_project_urls()
-for repo_name,url in zip(repo_names,repo_urls):
-    # Delete previous repo
-    if os.path.isdir(temp_repo_path):
-        remove_project(temp_repo_path)
+for repo_name, url in zip(repo_names, repo_urls):
     # Clone repo
     try:
         clone_project(url, temp_repo_path)
@@ -25,16 +23,19 @@ for repo_name,url in zip(repo_names,repo_urls):
         code_metrics = get_static_features(abs_filepaths)
         for filename in code_metrics:
             df.append([url,
-                        filename,
-                        code_metrics[filename]['loc'],
-                        code_metrics[filename]['lloc'],
-                        code_metrics[filename]['sloc'],
-                        code_metrics[filename]['comments'],
-                        code_metrics[filename]['multi'],
-                        code_metrics[filename]['blank'],
-                        code_metrics[filename]['single_comments'],
-                        code_metrics[filename]['avg_cc']])
+                filename,
+                code_metrics[filename]['loc'],
+                code_metrics[filename]['lloc'],
+                code_metrics[filename]['sloc'],
+                code_metrics[filename]['comments'],
+                code_metrics[filename]['multi'],
+                code_metrics[filename]['blank'],
+                code_metrics[filename]['single_comments'],
+                code_metrics[filename]['avg_cc']])
     except FileNotFoundError: pass
+    # Delete repo
+    if os.path.isdir(temp_repo_path):
+        remove_project(temp_repo_path)
 
 # Save dataframe as csv
 df = pd.DataFrame(df, columns=['url',
